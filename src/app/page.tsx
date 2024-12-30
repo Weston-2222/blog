ba2d2fe3e459eb3ca4522c88463bc0026303a4c2
weston-2222 <westonxie22@gmail.com>
2024-12-30 22:52:50 +0800

    feat: 新增3D模型

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import dynamic from 'next/dynamic';
+
+const CatModel = dynamic(
+  () =>
+    import(/* webpackChunkName:"CatModel" */ '@/components/catModel/catModel'),
+  {
+    loading: () => (
+      <div className='flex justify-center items-center h-[500px] w-[500px]'>
+        <div className='h-[100px] w-[100px] inline-block border-8 border-solid border-gray-200 rounded-full border-t-blue-500 animate-spin'></div>
+      </div>
+    ),
+  }
+);
 const page = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <p className='text-2xl font-bold'>正在建置中...</p>
+    <div>
+      <CatModel />
     </div>
   );
 };
