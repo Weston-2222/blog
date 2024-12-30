03887953afae68346d053eea43033b1ea1372254
weston-2222 <westonxie22@gmail.com>
2024-12-31 00:20:50 +0800

    feat: 調整樣式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const CatModel = dynamic(
     import(/* webpackChunkName:"CatModel" */ '@/components/catModel/catModel'),
   {
     loading: () => (
-      <div className='flex justify-center items-center h-full w-full'>
+      <div className='flex justify-center items-center h-[300px] w-full'>
         <div className='h-[100px] w-[100px] inline-block border-8 border-solid border-gray-200 rounded-full border-t-blue-500 animate-spin'></div>
       </div>
     ),
