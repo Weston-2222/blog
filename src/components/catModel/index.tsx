5d8495c870802e749bd183befbac09c44168396c
weston-2222 <westonxie22@gmail.com>
2024-12-31 16:19:04 +0800

    feat: 加入loading元件

--- src/components/catModel/index.tsx
+++ src/components/catModel/index.tsx
@@ -2,17 +2,14 @@
 import 'client-only';
 
 import dynamic from 'next/dynamic';
+import SpinnerLoading from '../spinnerLoading';
 
 const CatModel = dynamic(
   () =>
     import(/* webpackChunkName:"CatModel" */ '@/components/catModel/catModel'),
   {
     ssr: false,
-    loading: () => (
-      <div className='flex justify-center items-center md:h-[360px] h-[250px] w-full'>
-        <div className='h-[100px] w-[100px] inline-block border-8 border-solid border-gray-200 rounded-full border-t-blue-500 animate-spin'></div>
-      </div>
-    ),
+    loading: () => <SpinnerLoading className='h-[360px] w-full' />,
   }
 );
 
