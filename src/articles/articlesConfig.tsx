7d66ee859917356f19bc99ac82f7217087f3db1b
weston-2222 <westonxie22@gmail.com>
2025-01-01 23:49:27 +0800

    feat: 加入提示

--- src/articles/articlesConfig.tsx
+++ src/articles/articlesConfig.tsx
@@ -10,9 +10,15 @@ export type ArticleMetadata = Metadata & {
 export const articlePath = 'src/articles';
 
 export const articlesMetadataPromise: Promise<ArticleMetadata[]> = Promise.all([
-  import('@/articles/hello.mdx'),
-  import('@/articles/hello-world.mdx'),
   import('@/articles/test.mdx'),
 ]).then((modules) =>
   modules.map((module) => module.metadata as ArticleMetadata)
 );
+//metadata 的範例
+// export const metadata = {
+//   title: 'Post with code',
+//   description: 'My post with code',
+//   path: 'article',
+//   slug: 'hello-world',
+//   date: '2022-09-01',
+// };
