1fb5d0c0e0f5bc86b5c7d3523926fd0e4a8ababf
weston-2222 <westonxie22@gmail.com>
2025-01-09 13:39:11 +0800

    fix: 處理佔位符問題

--- src/articles/treejs-two-pitfalls/dynamic.tsx
+++ src/articles/treejs-two-pitfalls/dynamic.tsx
@@ -1,18 +1,13 @@
 'use client';
 import dynamic from 'next/dynamic';
+import './articleCar.css';
 import SpinnerLoading from '@/components/spinnerLoading';
 export const ExampleModel = dynamic(
   () => import(/*ExampleModel*/ './exampleModel'),
   {
     ssr: false,
     loading: () => (
-      <div
-        style={{
-          width: '100%',
-          height: window.innerWidth > 768 ? '357px' : '61vw',
-        }}
-        className='flex justify-center items-center'
-      >
+      <div className='flex justify-center items-center container'>
         <SpinnerLoading />
       </div>
     ),
